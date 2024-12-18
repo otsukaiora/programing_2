1281e84200fe228e2123d27b57a63a3a8698590d
otsukaiora <s2422104@stu.musashino-u.ac.jp>
2024-12-18 16:33:36 +0900

    大量のコミット

--- 2422104_5.sql
+++
@@ -1,5 +0,0 @@
-SELECT product_name, price
-FROM products
-WHERE category = 'Electronics'
-ORDER BY price DESC
-LIMIT 1;
--- 2422104_6.sql
+++
@@ -1,8 +0,0 @@
-SELECT customers.customer_name, SUM(products.price * orders.quantity) AS total_purchase
-FROM orders
-JOIN customers ON orders.customer_id = customers.customer_id
-JOIN products ON orders.product_id = products.product_id
-WHERE orders.order_date BETWEEN '2025-01-01' AND '2025-06-30'
-GROUP BY customers.customer_name
-ORDER BY total_purchase DESC
-LIMIT 3;
